c776d3b40d55022b90a00910b9629509910c0fcb
Caio Santos <95649452+New-Caiocolas@users.noreply.github.com>
2024-09-17 13:48:53 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+from tkinter import *
+
+# configurando janela
+root = Tk()
+root.title('Calculadora')
+root.geometry('640x679')
+root.maxsize(width='640', height='679')
+root.minsize(width='640', height='679')
+root.configure(bg='#D8D8D8')
+
+
+
+
+
+root.mainloop()
